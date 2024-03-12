9e9eefb43f5fcd0a634c6e5789038a88882fc954
RoAgens <r.konyaev@gmail.com>
2024-03-12 18:51:08 +0300

    Add project files.

---
+++ Command.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace AGRevitCommandSimple
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class Command : IExternalCommand
+    {
+        private Document _doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            _doc = commandData?.Application.ActiveUIDocument.Document;
+
+            try
+            {
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}

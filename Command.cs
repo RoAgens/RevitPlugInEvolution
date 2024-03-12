d9dbb21fa6fc330aaf93ab69b308d43c533c4575
RoAgens <r.konyaev@gmail.com>
2024-03-12 19:22:17 +0300

    add base abstract class

--- Command.cs
+++ Command.cs
@@ -1,27 +1,16 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using AGRevitCommandSimple.Base;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace AGRevitCommandSimple
 {
     [Transaction(TransactionMode.Manual)]
-    internal class Command : IExternalCommand
+    internal class Command : BaseExternalCommand
     {
-        private Document _doc;
-
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        public override Result Execute()
         {
-            _doc = commandData?.Application.ActiveUIDocument.Document;
-
-            try
-            {
-                return Result.Succeeded;
-            }
-            catch (Exception e)
-            {
-                message = e.Message;
-                return Result.Failed;
-            }
+            return Result.Succeeded;
         }
     }
 }

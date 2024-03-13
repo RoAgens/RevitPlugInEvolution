e0033bb8cfa093ff242f0bf37653b369e1df0025
RoAgens <r.konyaev@gmail.com>
2024-03-13 10:06:19 +0300

    time commit

---
+++ Commands/Base/BaseExternalCommand.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RVApplication = Autodesk.Revit.ApplicationServices.Application;
+
+namespace AGRevitCommandSimple.Base
+{
+    internal abstract class BaseExternalCommand : IExternalCommand
+    {
+        protected ExternalCommandData _commandData;
+        protected UIDocument _uiDoc;
+        protected RVApplication _app;
+        protected Document _doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            _commandData = commandData;
+            _uiDoc = commandData.Application.ActiveUIDocument;
+            _app = commandData.Application.Application;
+
+            _doc = commandData.Application.ActiveUIDocument?.Document;
+
+            try
+            {
+                return Execute();
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+
+        public abstract Result Execute();
+    }
+}
---
+++ Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using AGRevitCommandSimple.Base;
+using AGRevitCommandSimple.View;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace AGRevitCommandSimple
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class Command : BaseExternalCommand
+    {
+        public override Result Execute()
+        {
+            MainWindow mainWindow = new(_doc);
+            mainWindow.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}

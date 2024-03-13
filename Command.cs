39c097111f0ba723c0db281e823c91c8bcf5a134
RoAgens <r.konyaev@gmail.com>
2024-03-13 11:18:09 +0300

    add pick method in solution

--- Command.cs
+++ Command.cs
@@ -2,6 +2,7 @@
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 
 namespace AGRevitCommandSimple
 {
@@ -11,6 +12,34 @@ namespace AGRevitCommandSimple
         public override Result Execute()
         {
             return Result.Succeeded;
+        }
+
+        private string GetElementId()
+        {
+            string value = "";
+
+            Element elemnt = GetElement();
+
+            if (elemnt != null)
+                value = elemnt.Id.ToString();
+            else
+                value = "Element not selected";
+
+            return value;
+        }
+
+        private Element GetElement()
+        {
+            Element element = null;
+
+            Reference reference = _uiDoc.Selection.PickObject(ObjectType.Element, "Please pick any element");
+
+            if (reference != null)
+            {
+                element = _doc.GetElement(reference.ElementId);
+            }
+
+            return element;
         }
     }
 }
